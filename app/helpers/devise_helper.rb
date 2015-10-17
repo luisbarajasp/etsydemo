235423883af9d714fb6dabe24aa2a59069c29f6f
luisbarajasp <luisbarajasp@gmail.com>
2015-10-16 20:04:19 -0500

    Update devise

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,5 +1,5 @@
 module DeviseHelper
-	def devise_error_message!
+	def devise_error_messages!
 		return '' if resource.errors.empty?
 
 		messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
@@ -8,7 +8,7 @@ module DeviseHelper
 		html = <<-HTML
 		<div class="alert alert-danger alert-dismissable">
 			<button type="button" class="close" data-dismiss="alert" arida-hidden="true">&times;</button>
-			<h4>#{sentece}</h4>
+			<h4>#{sentence}</h4>
 			#{messages}
 		</div>
 		HTML
